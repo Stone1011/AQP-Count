a4483b8ea83058fa3f5f41df860eb5df37cb1412
Stone1011 <1943862547@qq.com>
2022-12-09 23:21:34 +0800

    Add auto-reconnecting

--- submit.py
+++ submit.py
@@ -10,25 +10,32 @@ url = "http://10.77.110.234:5000/result"
 #     'Accept-Encoding': 'gzip, deflate'
 # }
 
-def post(filePath):
+def wait(sec):
+    time.sleep(sec)
+
+def post(filePath, times, wait_time):
     file = {
         'input_file': open(filePath, 'rb')
     }
-    r = requests.post(url, files=file)
-    if(r.status_code == 200):
-        return r.text
-    else:
-        raise Exception('Fail to access the server')
 
-def submit(filePath):
-    text = post(filePath)
+    for i in range(times):
+        try:
+            r = requests.post(url, files=file)
+            if(r.status_code == 200):
+                return r.text
+            else:
+                raise Exception('Fail to access the server')
+        except Exception:
+            wait(time)
+
+    raise Exception('Fail to access the server')
+
+def submit(filePath, times=3, wait_time=3):
+    text = post(filePath, times, wait_time)
     soup = BeautifulSoup(text, "html.parser")
     ps = soup.find_all('p')
     result = ps[1].text.split(' ')[1]
     return result
-
-def wait(sec):
-    time.sleep(sec)
 
 value = submit('test_output.csv')
 print(value)
